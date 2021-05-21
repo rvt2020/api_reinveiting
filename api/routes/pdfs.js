08615a181ec16c662feb08d5b3541718b4ecd519
rvt2020 <rvt2020@reinventing.com.pe>
2021-05-21 10:32:35 -0500

    pdf

--- api/routes/pdfs.js
+++ api/routes/pdfs.js
@@ -7,22 +7,22 @@ const saltRounds = 10;
 module.exports = async app => {
   // para traer todos los usuarios
   app.get(
-    `/api/${process.env.VERSION}/pdf/actadeservicio/:orden`,
+    `/api/${process.env.VERSION}/pdf/js_acta_operac/:co_operac`,
     async (req, res, next) => {
       try {
         let query;
-        const doc_ide = req.params.orden;
-        query = `select * from pbperson.sp_mostrar_pernat('${doc_ide}');`;
+        const co_operac = req.params.co_operac;
+        query = `select * from reoperac.f_js_acta_operac('${co_operac}');`;
         bitacora.control(query, req.url);
-        const personas = await BD.storePostgresql(query);
+        const operac = await BD.storePostgresql(query);
 
         // con esto muestro msj
-        if (personas.codRes != 99) {
+        if (operac.codRes != 99) {
           // con esto muestro msj
-          res.json({ res: "ok", message: "Success", personas }).status(200);
+          res.json({ res: "ok", message: "Success", operac }).status(200);
         } else {
           res
-            .json({ res: "ko", message: "Error en la query", personas })
+            .json({ res: "ko", message: "Error en la query", operac })
             .status(500);
         }
       } catch (error) {
