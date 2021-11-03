2e1037294410e6ff53ea461a54bbc20d6a2ac9b0
rvt2020 <rvt2020@reinventing.com.pe>
2021-11-03 01:25:44 -0500

    carta_carateristicas

--- api/routes/pdfs.js
+++ api/routes/pdfs.js
@@ -5,7 +5,7 @@ const saltRounds = 10;
 // ``
 
 module.exports = async app => {
-  // Extraer Acta 
+  // ACTAS  
   app.get(
     `/api/${process.env.VERSION}/pdf/js_acta_operac/:co_operac`,
     async (req, res, next) => {
@@ -33,22 +33,19 @@ module.exports = async app => {
       }
     }
   )
-
+  
+  // CARTA DE CARACTERÍSTICAS  
   app.get(
-    `/api/${process.env.VERSION}/pdf/js_proforma_operac/:co_operac`,
+    `/api/${process.env.VERSION}/pdf/js_carta_caracteristica/:co_operac`,
     async (req, res, next) => {
       try {
         let query;
         const co_operac = req.params.co_operac;
-        query = `select * from reoperac.f_js_proforma_operac('${co_operac}');`;
+        query = `select * from reoperac.f_js_carta_caract('${co_operac}');`;
         bitacora.control(query, req.url);
         const operac = await BD.storePostgresql(query);
 
-        // Se entrega el vehículo en funcionamiento y en buenas condiciones por los trabajos 
-        // realizados en el taller. 
-        // En señal de conformidad del servicio, el cliente firma el presente documento ....fecha...
-        
-        if (operac.codRes != 99) {
+         if (operac.codRes != 99) {
           // con esto muestro msj
           res.json({ res: "ok", message: "Success", operac }).status(200);
         } else {
@@ -62,6 +59,33 @@ module.exports = async app => {
     }
   )
 
+
+  // PROFORMAS
+  app.get(
+    `/api/${process.env.VERSION}/pdf/js_proforma_operac/:co_operac`,
+    async (req, res, next) => {
+      try {
+        let query;
+        const co_operac = req.params.co_operac;
+        query = `select * from reoperac.f_js_proforma_operac('${co_operac}');`;
+        bitacora.control(query, req.url);
+        const operac = await BD.storePostgresql(query);
+
+        if (operac.codRes != 99) {
+          // con esto muestro msj
+          res.json({ res: "ok", message: "Success", operac }).status(200);
+        } else {
+          res
+            .json({ res: "ko", message: "Error en la query", operac})
+            .status(500);
+        }
+      } catch (error) {
+        res.json({ res: "ko", message: "Error controlado", error }).status(500);
+      }
+    }
+  )
+  
+  // ORDEN DE COMPRA
   app.get(
     `/api/${process.env.VERSION}/pdf/f_js_docume_ordcom/:co_ordcom`,
     async (req, res, next) => {
@@ -72,10 +96,6 @@ module.exports = async app => {
         bitacora.control(query, req.url);
         const ordcom = await BD.storePostgresql(query);
 
-        // Se entrega el vehículo en funcionamiento y en buenas condiciones por los trabajos 
-        // realizados en el taller. 
-        // En señal de conformidad del servicio, el cliente firma el presente documento ....fecha...
-        
         if (ordcom.codRes != 99) {
           // con esto muestro msj
           res.json({ res: "ok", message: "Success", ordcom }).status(200);
